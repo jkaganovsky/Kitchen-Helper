d19dd5b58dce045e5ea48e9ecc848e11880b2c5c
Carlos Brand <carlosbrand@Carloss-iMac.local>
2020-11-19 22:53:47 -0800

    3 APIs and modified functions with user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,25 +20,25 @@ function getRecipesbyIngredients(){
 function getRecipeInstructions(recipeId){
 
     var queryURL = "https://api.spoonacular.com/recipes/" + recipeId + "/analyzedInstructions?&apiKey=24796d1ddf8d413ba7e254114bc839c3";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-    });
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+        });
 }
 getRecipeInstructions();
 
 function getNutrition(ingredient1,ingredient2,ingredient3){
 
     var secondQueryURL = "https://api.edamam.com/api/nutrition-data?app_id=17b937a5&app_key=02f27b66f18bc4bf93156f026eabe8f8&ingr=" + num1 + "%20" + "medium" + ingredient1 + "%2C" + num2 + "%20"+ "medium" + "%20" + ingredient2 + "%2C" + num3 + "%20" + "medium" + "%20" + ingredient3
-    $.ajax({
-        url:secondQueryURL,
-        method:"GET"
+        $.ajax({
+            url:secondQueryURL,
+            method:"GET"
 
-    }).then(function(response){
-        console.log(response);
-    });
-}
+        }).then(function(response){
+            console.log(response);
+        });
+};
 
 // getNutrition();
