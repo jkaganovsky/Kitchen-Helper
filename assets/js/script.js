85e83a6e4da626f5b24f3c9ccea829473b1fbc0d
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2020-11-19 22:37:31 -0800

    Added recipe instructions in js & removed script from html.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
-
 function getRecipesbyIngredients(){
-    var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=ba1a88bc885e47ad9ebb5dd9c219d86d";
+    var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=24796d1ddf8d413ba7e254114bc839c3";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        $("instructions").attr("src", response[0].image);
+        console.log(response[0].id);
+    });
+
+}
+
+getRecipesbyIngredients();
+
+
+function getRecipeInstructions(){
+    var queryURL = "https://api.spoonacular.com/recipes/324694/analyzedInstructions?&apiKey=24796d1ddf8d413ba7e254114bc839c3";
 
     $.ajax({
         url: queryURL,
@@ -10,17 +26,23 @@ function getRecipesbyIngredients(){
     });
 
 }
-// getRecipesbyIngredients();
+
+getRecipeInstructions();
+
 
 function getNutrition(){
 
-    var secondQueryURL = "https://api.edamam.com/api/nutrition-data?app_id=17b937a5&app_key=02f27b66f18bc4bf93156f026eabe8f8&ingr=1%20large%20apple"
+    var secondQueryURL = "https://api.edamam.com/api/nutrition-data?app_id=17b937a5&app_key=02f27b66f18bc4bf93156f026eabe8f8&ingr=1%20large%20apple&apiKey=3d8504ff72124b3790e1881e4619a59c"
     $.ajax({
         url:secondQueryURL,
         method:"GET"
 
     }).then(function(response){
-        console.log(response);
+         console.log(response);
+        $("#calories").text("Calories: "+ response.totalNutrientsKCal.ENERC_KCAL.quantity + "kcal");
+        $("#carbs").text("Carbohydrates: "+ response.totalNutrientsKCal.CHOCDF_KCAL.quantity + "g");
+        $("#fat").text("Fat: "+ response.totalNutrientsKCal.FAT_KCAL.quantity + "g");
+        $("#protein").text("Protein: "+ response.totalNutrientsKCal.PROCNT_KCAL.quantity + "g");
     });
 }
 
