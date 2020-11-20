28f4687ecc8b44e00c64cf5e65b7d5022e6790aa
francine babauta <francine.babauta@outlook.com>
2020-11-20 10:40:31 -1000

    added input text field and buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-
-function getRecipesbyIngredients(){
+  function getRecipesbyIngredients(){
     var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=ba1a88bc885e47ad9ebb5dd9c219d86d";
 
     $.ajax({
