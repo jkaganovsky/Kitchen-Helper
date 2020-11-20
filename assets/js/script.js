3a012d726ba4a1511e653280d4cd791f0dec8a47
Elizabeth Brandt <ebrandt8@uw.edu>
2020-11-19 22:15:15 -0800

    added classes to recipe tiles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,29 @@
 
-function getRecipesbyIngredients(){
-    var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=ba1a88bc885e47ad9ebb5dd9c219d86d";
+// function getRecipesbyIngredients(){
+//     var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=ba1a88bc885e47ad9ebb5dd9c219d86d";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-    });
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response){
+//         console.log(response);
+//     });
 
-}
-// getRecipesbyIngredients();
+// }
+// // getRecipesbyIngredients();
 
-function getNutrition(){
+// function getNutrition(){
 
-    var secondQueryURL = "https://api.edamam.com/api/nutrition-data?app_id=17b937a5&app_key=02f27b66f18bc4bf93156f026eabe8f8&ingr=1%20large%20apple"
-    $.ajax({
-        url:secondQueryURL,
-        method:"GET"
+//     var secondQueryURL = "https://api.edamam.com/api/nutrition-data?app_id=17b937a5&app_key=02f27b66f18bc4bf93156f026eabe8f8&ingr=1%20large%20apple"
+//     $.ajax({
+//         url:secondQueryURL,
+//         method:"GET"
 
-    }).then(function(response){
-        console.log(response);
-    });
-}
+//     }).then(function(response){
+//         console.log(response);
+//     });
+// }
 
-getNutrition();
+// getNutrition();
+
+var ileRows = $()
